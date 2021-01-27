31e0c2d0d046ad799f31d844bfc0a98ff38f35e0
Julio Palencia <julio2201@gmail.com>
2021-01-27 15:25:53 -0600

    Added Redux Toolkit and _document

---
+++ src/redux/slices/test.ts
@@ -0,0 +1,12 @@
+// Packages
+import { createSlice } from '@reduxjs/toolkit';
+
+const testSlice = createSlice({
+	name: 'test',
+	initialState: [],
+	reducers: {}
+});
+
+export const {} = testSlice.actions;
+
+export default testSlice.reducer
