6f703a55072114c1b979f65e892a35c53b90ea81
smerida <smerida@autoweb.com>
2021-01-27 10:49:30 -0600

    MODIFIED: Footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,17 +1,72 @@
 // Components
 import Container from '@/comp/container';
+import Row from '@/comp/container/row';
+import Column from '@/comp/container/column';
 
 // Utilities
 import getYear from '@/util/get-year';
 
+// Definitions
+import { IFooter } from '@/def/IFooter';
+
 // Styles
-import { FooterWrapper } from './style';
+import { FooterWrapper, FooterContainer, FooterList, FooterAnchor, FooterText } from './style';
+
+const FirstColumn = [
+	{ url: '/', title: 'About Us' },
+	{ url: '/', title: 'Contact Us' },
+	{ url: '/', title: 'Privacy Policy' },
+	{ url: '/', title: 'Do NOT Sell my Personal Information' }
+];
+const SecondColumn = [
+	{ url: '/', title: 'Terms of User' },
+	{ url: '/', title: 'Fraud Awareness' },
+	{ url: '/', title: 'Advice' },
+	{ url: '/', title: 'Best Cars' }
+];
+const ThirdColumn = [
+	{ url: '/', title: 'Calculators' },
+	{ url: '/', title: 'Sitemap' }
+];
 
 const Footer: React.FC = () => {
 	const year = getYear();
 	return (
 		<FooterWrapper>
-			<p>Footer change</p>
+			<Container>
+				<Row>
+					<Column sm={1} md={3}>
+						<FooterContainer>
+							{FirstColumn.map((item: IFooter, index: number) => (
+								<FooterList key={index}>
+									<FooterAnchor href={item.url}>{item.title}</FooterAnchor>
+								</FooterList>
+							))}
+						</FooterContainer>
+					</Column>
+					<Column sm={1} md={3}>
+						<FooterContainer>
+							{SecondColumn.map((item: IFooter, index: number) => (
+								<FooterList key={index}>
+									<FooterAnchor href={item.url}>{item.title}</FooterAnchor>
+								</FooterList>
+							))}
+						</FooterContainer>
+					</Column>
+					<Column sm={1} md={3}>
+						<FooterContainer>
+							{ThirdColumn.map((item: IFooter, index: number) => (
+								<FooterList key={index}>
+									<FooterAnchor href={item.url}>{item.title}</FooterAnchor>
+								</FooterList>
+							))}
+						</FooterContainer>
+					</Column>
+					<Column sm={1} md={1}>
+						<FooterText>© {year} AutoWeb, Inc.</FooterText>
+					</Column>
+				</Row>
+			</Container>
 		</FooterWrapper>
 	);
 };
