7ab7284ff21eac0bce8a9b66d7e22ee9ab6e0304
Julio Palencia <julio2201@gmail.com>
2021-02-01 15:14:48 -0600

    Added form

--- src/components/by-makes/index.tsx
+++ src/components/by-makes/index.tsx
@@ -6,9 +6,20 @@ import Row from '@comp/container/row';
 import Column from '@comp/container/column';
 import Display from '@comp/container/display';
 import MakeList from "@comp/make-list";
+import Button from '@comp/button';
+import Input from '@comp/form-elements/input';
+import Select from '@comp/form-elements/select';
 
 // Styles
 import { ByMakesWrapper, ByMakesTitle, ByMakesImg, ByMakesSideTitle, ByMakesSideText } from './style';
+
+const options = [
+	{id: '1', name: 'Name 1', value: 'value-1'},
+	{id: '2', name: 'Name 2', value: 'value-2'},
+	{id: '3', name: 'Name 3', value: 'value-3'},
+	{id: '4', name: 'Name 4', value: 'value-4'},
+	{id: '5', name: 'Name 5', value: 'value-5'}
+];
 
 const ByMakes: React.FC = () => {
 	return (
@@ -23,6 +34,27 @@ const ByMakes: React.FC = () => {
 						<ByMakesImg src="/assets/img/makes-image.jpg" alt="Makes Image" />
 						<ByMakesSideTitle>Because you know or like the manufacturer</ByMakesSideTitle>
 						<ByMakesSideText>It’s easier to find your next car when you already know what you’re looking for. Try searching the option below.</ByMakesSideText>
+						<form>
+							<Select
+								id="make"
+								value=""
+								name="make"
+								label="Make"
+								error={false}
+								message="Please select a"
+								options={options}
+							/>
+							<Select
+								id="model"
+								value=""
+								name="model"
+								label="Model"
+								error={false}
+								message="Please select a"
+								options={options}
+							/>
+							<Button>Begin Search</Button>
+						</form>
 					</Display>
 				</Column>
 			</Row>
