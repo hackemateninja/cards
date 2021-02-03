3c3f8b45fc5e6a7272a4f6c95dd915983752c785
smerida <smerida@autoweb.com>
2021-02-03 14:53:34 -0600

    MODIFIED: SRP Added

---
+++ pages/buy/index.tsx
@@ -0,0 +1,41 @@
+// Packages
+import { ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+
+// Layout
+import DefaultLayout from '@layout/default';
+
+//Components
+import Container from '@comp/container';
+import Row from '@comp/container/row';
+import Column from '@comp/container/column';
+
+// Styles
+import GlobalStyles from '@theme/global';
+import PrimaryTheme from '@theme/primary';
+
+const Buy: React.FC = ( props ) => {
+	
+	return (
+		<ThemeProvider theme={PrimaryTheme}>
+			<Head>
+				<title>Buy Used Cars - Shop Used Cars</title>
+			</Head>
+			<GlobalStyles />
+			<DefaultLayout>
+				<Container>
+                    <Row>
+                        <Column xs={1} md={4}>
+                            <p>Filters</p>
+                        </Column>
+                        <Column xs={1} md='auto'>
+                            <p>Listings</p>
+                        </Column>
+                    </Row>
+				</Container>		
+			</DefaultLayout>
+		</ThemeProvider>
+	);
+};
+
+export default Buy;
